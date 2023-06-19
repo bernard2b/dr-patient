84a7b18f94deda98f20bff118e10d2b7b7ba600d
bernard.gyugyi <bernard.gyugyi@gmail.com>
2023-06-19 11:20:02 +0200

    add one-to-many relation on doctors table

--- src/main/java/com/example/dr_app/model/Patient.java
+++ src/main/java/com/example/dr_app/model/Patient.java
@@ -1,6 +1,5 @@
 package com.example.dr_app.model;
 
-import jakarta.persistence.Table;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,6 +7,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -58,5 +58,10 @@ public class Patient {
             unique = true
     )
     private String email;
-    private Long doctor_id;
+
+    private Long dr_patient_fk;
+
+    public Patient() {
+        // Default constructor
+    }
 }
